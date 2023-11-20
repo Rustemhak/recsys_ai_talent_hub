04eaf1d5e6ec7302d1f58397b97e7a7141c22e45
Rustemhak <rustemhak@mail.ru>
2023-11-20 15:05:58 +0300

    add hw1

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 import pytest
-from httpx import AsyncClient
+from starlette.testclient import TestClient
 
 from service.api.app import create_app
 from service.api.exceptions import UserNotAuthorized
@@ -10,19 +10,18 @@ from service.settings import get_config
 
 config = get_config()
 app = create_app(config)
-HEADERS = {"Authorization": f"Bearer {SECRET_PHRASE}"}
+
+client = TestClient(app)
 
 
-@pytest.mark.asyncio
-async def test_authorized_request():
-    async with AsyncClient(app=app, base_url="http://localhost") as client:
-        response = await client.get("/health", headers=HEADERS)
-        assert response.status_code == HTTPStatus.OK
+def test_authorized_request():
+    headers = {"Authorization": f"Bearer {SECRET_PHRASE}"}
+    response = client.get("/health", headers=headers)
+    assert response.status_code == HTTPStatus.OK
 
 
-@pytest.mark.asyncio
-async def test_unauthorized_request():
-    async with AsyncClient(app=app, base_url="http://localhost") as client:
-        with pytest.raises(UserNotAuthorized) as exc_info:
-            await client.get("/health", headers=HEADERS)
-        assert exc_info.value.status_code == HTTPStatus.UNAUTHORIZED
+def test_unauthorized_request():
+    headers = {"Authorization": "Bearer invalid_token"}
+    with pytest.raises(UserNotAuthorized) as exc_info:
+        client.get("/health", headers=headers)
+    assert exc_info.value.status_code == HTTPStatus.UNAUTHORIZED
